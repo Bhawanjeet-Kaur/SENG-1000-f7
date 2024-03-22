e2605bf55bbdcd64ef761c49840d8cc99b3d08a5
Bhawanjeet Kaur <bhawanjeetgill@gmail.com>
2024-03-22 15:41:28 -0400

    First commit main function created

---
+++ f7/f7/f7.cpp
@@ -0,0 +1,13 @@
+/*
+* FILE : f7.cpp
+* PROJECT : SENG1000- focused assignment 7
+* PROGRAMMER : Bhawanjeet Kaur Gill
+* FIRST VERSION : 2024-03-22
+* DESCRIPTION : Write a program to give you practice at creating text and binary files.
+*/
+#include <stdio.h>
+#pragma warning(disable: 4996)
+int main(void)
+{
+	return 0;
+}
